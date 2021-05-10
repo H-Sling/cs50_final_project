f30f67d6a54af634e4e300efb0a398c801fc8cf5
Harry Slingsby <slingsby.h.i@gmail.com>
2021-05-10 19:02:25 +0100

    added + new line button and have a global variable to keep count of the number of lines

--- gui_poc.py
+++ gui_poc.py
@@ -1,7 +1,4 @@
-# TODO SUnday 9th May - get dropdown of file extentions found in the root folder
-    # allow users to enter multiple file types and destinations and make the sort fucntion handle that. 
-    # ensure that the users entry is taken into the sort fucniton. 
-    # copy the existing destination frame to get the app working in POC form again
+# TODO - ensure that each new line created keeps count and names the elements uniquely 
 # TODO - Make a dropdown list autopopulate based on the file extentions found in source and
 # TODO - make it so that any unsorted file types simply are left in the root folder (maybe a warning?)
 # TODO - improve UI
@@ -16,6 +13,9 @@ import sorter
 from PIL import ImageTk, Image
 
 
+# Global Variables
+dst_count = 0
+
 # Create the window on the users screen
 window = tk.Tk()
 window.geometry("500x500")
@@ -24,12 +24,11 @@ window.title("Tidy App")
 #create the frames for the desired layout
 frame_header = tk.Frame(master = window, pady=2, padx=2)
 frame_src = tk.Frame(master = window, pady=2, padx=2)
-frame_inst2=tk.Frame(master=window, pady=1, padx=2)
-frame_dst1 = tk.Frame(master = window, pady=2, padx=2)
+frame_dst = tk.Frame(master = window, pady=2, padx=2)
 frame_button = tk.Frame(master = window, pady=2, padx=2)
 frame_header.grid(row=0, column=0)
 frame_src.grid(row=1, column=0)
-frame_dst1.grid(row=2, column=0)
+frame_dst.grid(row=2, column=0)
 frame_button.grid(row=3, column=0)
 
 # open the image file and resize so that it is about the size of a logo
@@ -81,48 +80,75 @@ browse_src.grid(row=0, column=1)
 source = tk.Entry(master=frame_src_entry, width=55)
 source.grid(row=0, column=0)
 
-# Get the user input for the source file when the button is pressed
-def get_dst1():
-    dst = filedialog.askdirectory()
-    dst1.insert(0, dst)
+# Define the lists of elements for the dst lines
+#frame_dst_
+
+def new_dst():
+    #referance global variables
+    global dst_count
+
+    # Get the user input for the source file when the button is pressed
+    def get_dst():
+        dst = filedialog.askdirectory()
+        dst1.insert(0, dst)
 
 
-# layout for the source section
-frame_dst1_inst = tk.Frame(frame_dst1)
-frame_dst1_entry = tk.Frame(frame_dst1)
-frame_dst1_inst.pack()
-frame_dst1_entry.pack()
+    # layout for the source section
+    frame_dst_inst = tk.Frame(frame_dst)
+    frame_dst_entry = tk.Frame(frame_dst)
+    frame_dst_inst.pack()
+    frame_dst_entry.pack()
 
-# Instructional Label
-dst1_label = tk.Label(frame_dst1_inst, text="Select the file type and destination folder!")
-dst1_label.pack()
+    # Instructional Label
+    dst1_label = tk.Label(frame_dst_inst, text="Select the file type and destination folder!")
+    dst1_label.pack()
 
-# Placeholder for file type dropdown
-file_type1 = tk.Entry(frame_dst1_entry, width=5)
-file_type1.grid(row=0, column=0)
+    # Placeholder for file type dropdown
+    file_type1 = tk.Entry(frame_dst_entry, width=5)
+    file_type1.grid(row=0, column=0)
 
-#button for the user to choose the source file to sort. 
-browse_dst1 = tk.Button(
-    master=frame_dst1_entry,
-    text="Browse",
+    #button for the user to choose the source file to sort. 
+    browse_dst1 = tk.Button(
+        master=frame_dst_entry,
+        text="Browse",
+        width=7,
+        height=1,
+        bg="#D4D0C8",
+        fg="black", 
+        borderwidth=5,
+        relief=tk.RAISED, 
+        command=get_dst
+    )
+    browse_dst1.grid(row=0, column=2)
+
+    # Create a label to store the Source path in; TODO - make the label responsive to the text
+    dst1 = tk.Entry(frame_dst_entry, width=50)
+    dst1.grid(row=0, column=1)
+
+    # Keep count of the number of dsts added - NOT WORKING!
+    dst_count = dst_count + 1
+
+new_dst()
+
+# New Dst button - allows the user to add a line in the visual code
+sort = tk.Button(
+    master=frame_button,
+    text="+ file type",
     width=7,
     height=1,
     bg="#D4D0C8",
     fg="black", 
     borderwidth=5,
     relief=tk.RAISED, 
-    command=get_dst1
+    command=new_dst  
 )
-browse_dst1.grid(row=0, column=2)
-
-# Create a label to store the Source path in; TODO - make the label responsive to the text
-dst1 = tk.Entry(frame_dst1_entry, width=50)
-dst1.grid(row=0, column=1)
-
+sort.pack()
 # On click - trigger the sort fucntion
 # NOTE: Does this fucntion need to be triggered mutliple times and only handle one file type and destination at a time?
 def handle_click():
-    sorter.Sort(source.get(), dst1.get(), file_type1.get())
+    print(dst_count)
+    for i in range(dst_count):
+        sorter.Sort(source.get(), dst1.get(), file_type1.get())
 
 # Sort button 
 sort = tk.Button(
